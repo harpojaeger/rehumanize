61738c753894fac090f5c9560bbb17753ae235d8
Harpo Jaeger <harpo.jaeger@gmail.com>
2021-12-14 20:14:24 -0500

    fix typo

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rehumanize',
+    version='0.1.0',
+    description='make numbers sound cooler',
+    # long_description=,
+    author='Harpo jaeger',
+    author_email='harpo.jaeger@gmail.com',
+    url='https://github.com/harpojaeger/rehumanize',
+    # license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
--- tests/test_trivial.py
+++ tests/test_trivial.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-# Add the relatie path to the module to the py PATH. This should allow us to to
+# Add the relative path to the module to the py PATH. This should allow us to to
 # import the module without needing to specify a relative path for it.
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
 import rehumanize
