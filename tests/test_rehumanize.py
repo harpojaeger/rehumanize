f516d4a213312d3de7855869696cda1cadcf4eba
Harpo Jaeger <harpo.jaeger@gmail.com>
2021-12-14 20:42:04 -0500

    import only the needed method

--- tests/test_rehumanize.py
+++ tests/test_rehumanize.py
@@ -5,13 +5,13 @@ import unittest
 # Add this file's parent directory to the py PATH. This should allow us to
 # import the module without needing to specify a relative path for it.
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
-import rehumanize
+from rehumanize import rehumanize
 
 
 class BasicTest(unittest.TestCase):
 
   def test_basic(self):
-    self.assertEqual(rehumanize.rehumanize(1), "one")
+    self.assertEqual(rehumanize(1), "one")
 
 
 if __name__ == '__main__':
