17e3e0aa96261ebf083f6ae9c223a49504fa08c1
Harpo Jaeger <harpo.jaeger@gmail.com>
2021-12-15 19:21:49 -0500

    start writing business logic with lt twenty numbers

--- rehumanize/rehumanize.py
+++ rehumanize/rehumanize.py
@@ -1,2 +1,10 @@
+LT_TWENTY = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+             "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+TENS = ["twenty", "thirty", "forty", "fifty",
+        "sixty", "seventy", "eighty", "ninety"]
+THOUSANDS = ["thousand", "million", "billion"]
+
+
 def rehumanize(num: int) -> str:
-  return "flooby"
+    if num < 20:
+        return LT_TWENTY[num]
--- tests/test_rehumanize.py
+++ tests/test_rehumanize.py
@@ -2,16 +2,25 @@ import os
 import sys
 import unittest
 
-# Add this file's parent directory to the py PATH. This should allow us to
-# import the module without needing to specify a relative path for it.
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
+from parameterized import parameterized
 from rehumanize import rehumanize
 
+# Add this file's parent directory to the py PATH. This should allow us to
+# import the module without needing to specify a relative path for it.
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..')))
 
-class BasicTest(unittest.TestCase):
 
-  def test_basic(self):
-    self.assertEqual(rehumanize(1), "one")
+class RehumanizeTest(unittest.TestCase):
+
+    @parameterized.expand([
+        (2, "two"),
+        (10, "ten"),
+        (12, "twelve"),
+        (20, "twenty"),
+        (95, "ninety-five")])
+    def test_s(self, num, string):
+        self.assertEqual(rehumanize(num), string)
 
 
 if __name__ == '__main__':
