8e49c643ca9878b3525a2deed66ab269e71707f8
Harpo Jaeger <harpo.jaeger@gmail.com>
2021-12-23 19:08:36 -0500

    clarify some naming; inlining TK

--- rehumanize/rehumanize.py
+++ rehumanize/rehumanize.py
@@ -30,14 +30,14 @@ def rehumanize(num: int) -> str:
         group_written_form: str = ""
         if group_value > 0:
             # Prepend a separator to the written form of this group _only_ if
-            # another group will end up following it.
+            # another group will end up following (i.e. to the left of) it.
             if len(digits) > 0:
                 if group_value >= 100 or num_groups > 0:
                     group_written_form = ", "
                 else:
                     group_written_form = " and "
 
-            group_written_form += process_three_digit_number(group_value)
+            group_written_form += three_digit_prefix(group_value)
 
             if num_groups > 0:
                 group_written_form += " " + THOUSANDS[num_groups-1]
@@ -49,9 +49,9 @@ def rehumanize(num: int) -> str:
     return written_form
 
 
-def process_three_digit_number(num: int) -> str:
+def three_digit_prefix(num: int) -> str:
     two_digits: int = num % 100
-    written_form = process_two_digit_number(two_digits)
+    written_form: int = process_two_digit_number(two_digits)
     if num < 100:
         return written_form
 
@@ -62,7 +62,7 @@ def process_three_digit_number(num: int) -> str:
         written_form = f" and {written_form}"
 
     hundreds_place: int = int((num - two_digits)/100)
-    written_form: str = f"{LT_TWENTY[hundreds_place]} hundred{written_form}"
+    written_form = f"{LT_TWENTY[hundreds_place]} hundred{written_form}"
 
     return written_form
 
